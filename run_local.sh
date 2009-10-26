cadb26fb75d3e4aec47124be7d7591627c9a0afc
Enrico Tröger <enrico.troeger@uvena.de>
2009-10-26 22:11:30 +0100

    Add the -c command line option to the test script

--- run_local.sh
+++ run_local.sh
@@ -5,4 +5,4 @@ export PYTHONPATH="include:$PYTHONPATH"
 program=`pwd`"/bin/yaturl_service.py"
 
 #~ python2.5 bin/yaturl_service.py -f
-python2.5 $program -f
+python2.5 $program -f -c /opt/projects/yaturl.conf
