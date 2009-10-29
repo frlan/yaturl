7377b2158a33c7dd7ef9f3433fc79942cee4e059
Frank Lanitz <frank@frank.uvena.de>
2009-10-29 22:25:56 +0100

    Show new URL on result page as well as make it click able

--- include/yaturl/requesthandler.py
+++ include/yaturl/requesthandler.py
@@ -76,15 +76,18 @@ class YuRequestHandler(BaseHTTPRequestHandler):
             # Begin the response
             self.send_response(200)
             if not self.server.db.is_hash_in_db(hash):
+                short = self.server.db.add_link_to_db(hash, form['URL'].value)
+                new_URL= '<a href="http://yaturl.net/%s">http://yaturl.net/%s</a>' % (short,short)
                 text = yaturlTemplate.template(
                        self.server.config.get('templates','staticresultpage'),
-                       URL=self.server.db.add_link_to_db(hash, form['URL'].value))
+                       URL=new_URL)
             else:
                 # It appears link is already stored or you have found an collision on sha1
+                short = self.server.db.get_short_for_hash_from_db(hash)[0]
+                new_URL= '<a href="http://yaturl.net/%s">http://yaturl.net/%s</a>' % (short,short)
                 text = yaturlTemplate.template(
-                    self.server.config.get('templates','staticresultpage'),
-                    URL=self.server.db.get_short_for_hash_from_db(hash)[0])
-
+                       self.server.config.get('templates','staticresultpage'),
+                       URL=new_URL)
         else:
             text = yaturlTemplate.template(
             self.server.config.get('templates','statichomepage'),
