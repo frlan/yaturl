3c0c74a6dc4183d08bf7f01e6afeded61491e360
Frank Lanitz <frank@frank.uvena.de>
2009-12-27 20:13:32 +0100

    Remove stupid double defintion of function

--- include/yaturl/requesthandler.py
+++ include/yaturl/requesthandler.py
@@ -94,13 +94,6 @@ class YuRequestHandler(BaseHTTPRequestHandler):
         self.send_header('Content-type', 'text/html')
         self.end_headers()
     #----------------------------------------------------------------------
-    def _send_301(self, new_url):
-        self.send_response(301)
-        self.send_header('Location', new_url)
-        self.send_header('Content-type', 'text/html')
-        self.end_headers()
-
-    #----------------------------------------------------------------------
     def _send_404(self, header_only=False):
         text = yaturlTemplate.template(
             self.server.config.get('templates','corruptlink'),
