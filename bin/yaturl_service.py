46c9e881e162e719840aa3c190f095727000b749
Enrico Tröger <enrico.troeger@uvena.de>
2010-07-18 22:57:42 +0200

    Add an interactive telnet console to access some of the internals during runtime
    With the help of the TelnetInteractiveConsoleServer class of the bugger
    package (http://github.com/posborne/bugger), it's easy to embed
    an interactive Python shell accessable via telnet.

--- bin/yaturl_service.py
+++ bin/yaturl_service.py
@@ -1,8 +1,28 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+#
+# Author:  Enrico Tröger
+#          Frank Lanitz <frank@frank.uvena.de>
+# License: GPL v2 or later
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+# MA 02110-1301, USA.
 
 
 from yaturl.server import YuServer
+from console import TelnetInteractiveConsoleServer
 from ConfigParser import SafeConfigParser
 from optparse import OptionParser
 from signal import signal, SIGINT, SIGTERM
@@ -12,6 +32,11 @@ import logging
 import os
 import pwd
 import sys
+import threading
+
+
+TELNET_CONSOLE_SERVER = None
+HTTP_SERVER = None
 
 
 #----------------------------------------------------------------------
@@ -32,11 +57,17 @@ def setup_options(base_dir, parser):
         help=u'stay in foreground, do not daemonize')
 
 #----------------------------------------------------------------------
+def cleanup():
+    if TELNET_CONSOLE_SERVER:
+        TELNET_CONSOLE_SERVER.stop()
+    logging.shutdown()
+
+#----------------------------------------------------------------------
 def signal_handler(signum, frame):
     """
     On SIGTERM and SIGINT, cleanup and exit
     """
-    logging.shutdown()
+    cleanup()
     sys.exit(1)
 
 #----------------------------------------------------------------------
@@ -87,12 +118,25 @@ def setup_logging(config, name, fmt):
     return logger
 
 #----------------------------------------------------------------------
+def start_telnet_console(config, _locals):
+    global TELNET_CONSOLE_SERVER
+    if config.getboolean('telnet', 'enable'):
+        # fire up telnet server console
+        host = config.get('telnet', 'host')
+        port = config.getint('telnet', 'port')
+        TELNET_CONSOLE_SERVER = TelnetInteractiveConsoleServer(host=host, port=port, _locals=_locals)
+        console_thread = threading.Thread(target=TELNET_CONSOLE_SERVER.accept_interactions)
+        console_thread.start()
+
+#----------------------------------------------------------------------
 def main():
     """
     main()
 
     | **return** exit_code (int)
     """
+    global HTTP_SERVER
+
     base_dir = os.path.abspath('%s/..' % (os.path.dirname(__file__)))
 
     # arguments
@@ -137,12 +181,15 @@ def main():
 
     errorlog.info('Server started')
 
+    server = HTTP_SERVER = YuServer(config, errorlog, accesslog)
+
+    _locals = dict(server=server, config=config, errorlog=errorlog, accesslog=accesslog)
+    start_telnet_console(config, _locals)
+
     # loop forever listening to connections.
-    server = YuServer(config, errorlog, accesslog)
     server.serve_forever()
 
-    # clean up though we usually won't get here
-    logging.shutdown()
+    cleanup()
     exit(0)
 
 
