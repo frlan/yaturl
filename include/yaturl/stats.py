6761ee1eccf123a3655db7136bfef9207b044725
Frank Lanitz <frank@frank.uvena.de>
2011-01-01 01:22:37 +0100

    Update of copyright information for 2011

--- include/yaturl/stats.py
+++ include/yaturl/stats.py
@@ -2,7 +2,7 @@
 #
 #       stats.py
 #
-#       Copyright 2010 Frank Lanitz <frank(at)frank(dot)uvena(dot)de>
+#       Copyright 2010-2011 Frank Lanitz <frank(at)frank(dot)uvena(dot)de>
 #
 #       This program is free software; you can redistribute it and/or modify
 #       it under the terms of the GNU General Public License as published by
