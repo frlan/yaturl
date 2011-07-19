4e7983be109e1148b3fa6ddb9b593811eccd0223
Frank Lanitz <frank@frank.uvena.de>
2011-07-20 00:35:37 +0200

    Adjusting DOCTYPE-string for HTML5

--- source/server/include/yaturl/constants.py
+++ source/server/include/yaturl/constants.py
@@ -34,8 +34,7 @@ CONTENT_TYPES = ({
 })
 
 # we need to hard-code this one at least in case of the file cannot be found on disk
-TEMPLATE_500 = '''<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
-  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+TEMPLATE_500 = '''<!DOCTYPE HTML>
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 
 <head>
